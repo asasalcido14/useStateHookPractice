26c41b45bc1c44e0af94d66fbf97a0c1995b049d
asasalcido14 <asasalcido14@email.arizona.edu>
2022-01-30 17:59:02 -0700

    Functional React App
    added setInterval method with updateTime as key and 1000 as value - also updateTime function to set newTime to current time.

--- currtime-app/src/components/App.jsx
+++ currtime-app/src/components/App.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 
 function App() {
+	setInterval(updateTime, 1000);
+
 	const now = new Date().toLocaleTimeString();
 	//change continously so we need useState hook
 
 	const [time, setTime] = useState(now);
 	// first parameter is custom the other is from React library
-	console.log(now);
+	function updateTime() {
+		// we want to update time which is the "value"
+		const newTime = new Date().toLocaleDateString();
+		setTime(newTime);
+	}
+
 	return (
 		<div className="container">
 			<h1>{time}</h1>
-			<button>Get Time</button>
+			<button onClick={updateTime}>Get Time</button>
 		</div>
 	);
 }
